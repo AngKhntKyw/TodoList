514ee8f96ccdf8c7ffbc8a9b40abe1929a7eaaec
Aung Khant Kyaw <aungkhantkyaw8918.com>
2025-05-15 12:31:47 +0630

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Digital Base on 15/5/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
